c890dca44ecb54e23d6b7000c517da27e0c50618
jin-jeffrey <49298246+jin-jeffrey@users.noreply.github.com>
2020-07-13 01:36:53 -0400

    added carousel system

--- inventory/invapp/invapp/urls.py
+++ inventory/invapp/invapp/urls.py
@@ -18,6 +18,8 @@ from django.urls import include, path
 from users import views as user_views
 from inventory import views as inventory_views
 from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -33,3 +35,6 @@ urlpatterns = [
     path('item/<int:pk>/delete/', inventory_views.ItemDeleteView.as_view(), name = 'item-delete'),
     path('', inventory_views.about, name = 'about')
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
