d1de6a1a99811513637fe393983091e583fb47b1
jin-jeffrey <49298246+jin-jeffrey@users.noreply.github.com>
2020-07-14 18:24:49 -0400

    added password reset functionality

--- inventory/invapp/invapp/settings.py
+++ inventory/invapp/invapp/settings.py
@@ -134,3 +134,9 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = 'inventory'
 LOGIN_URL = 'login'
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('INVM_EMAIL_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('INVM_EMAIL_PASS')
